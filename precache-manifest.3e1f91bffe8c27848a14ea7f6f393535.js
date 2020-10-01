e8409278bb8cf10a332cf68d76bb15f508b81f77
Danish Riaz <danishriaz0609@outlook.com>
2020-10-01 10:48:03 +0500

    Updates

---
+++ precache-manifest.3e1f91bffe8c27848a14ea7f6f393535.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e8e7827d419526735958d86cd097617",
+    "url": "/job-board/index.html"
+  },
+  {
+    "revision": "848324d1ec3ab1f3e6d2",
+    "url": "/job-board/static/css/main.993bd101.chunk.css"
+  },
+  {
+    "revision": "192bf6e5938793d3ea63",
+    "url": "/job-board/static/js/2.4d64447d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/job-board/static/js/2.4d64447d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "848324d1ec3ab1f3e6d2",
+    "url": "/job-board/static/js/main.7b730cd4.chunk.js"
+  },
+  {
+    "revision": "1003b652c0c926895257",
+    "url": "/job-board/static/js/runtime-main.c4eeb53a.js"
+  }
+]);
